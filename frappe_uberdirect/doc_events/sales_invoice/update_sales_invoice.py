34c99e5ebd82c1f2bd3c09505de1d7d47129daae
dev-Anamul <ah9737677@gmail.com>
2026-01-29 12:23:55 +0000

    change in update sales invoice

--- frappe_uberdirect/doc_events/sales_invoice/update_sales_invoice.py
+++ frappe_uberdirect/doc_events/sales_invoice/update_sales_invoice.py
@@ -13,12 +13,22 @@ def update_sales_invoice(doc, method):
     Update sales invoice document events.
     """
 
+    frappe.log_error("Update sales invoice", f"Update sales invoice: {doc.name}")
+
     # if service type is not Delivery, return
     if doc.custom_service_type != "Delivery":
+        frappe.log_error(
+            "Service type is not Delivery",
+            f"Service type is not Delivery: {doc.custom_service_type}",
+        )
         return
 
     # if order status is not Accepted, return
     if doc.custom_order_status != "In kitchen":
+        frappe.log_error(
+            "Order status is not In kitchen",
+            f"Order status is not In kitchen: {doc.custom_order_status}",
+        )
         return
 
     # this is a delivery order, so we need to start delivery proccess
